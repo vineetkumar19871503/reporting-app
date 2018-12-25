7d1652d7b4118ba9fc2da0b93b89be09012a818a
Vineet Kumar <vineet.kumar198715@gmail.com>
2018-12-25 23:18:08 +0530

    working

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 // let env = process.env;
-// console.log(env);
 module.exports = {
     "apiUrl": "https://my-reporting-app-api.herokuapp.com/api/v1/",
     // "apiUrl": "http://localhost:3005/api/v1/"
